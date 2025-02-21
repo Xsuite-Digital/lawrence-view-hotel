594436d234b7824a22c8de79bc3e0f8addaf9c65
Muhammad Anas <41787190+Anaskhaan@users.noreply.github.com>
2025-02-21 15:54:56 +0500

    initaial configuration done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import AboutUs from "./pages/AboutUs";
+import Contact from "./pages/Contact";
+import AppLayout from "./components/AppLayout";
 
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<AppLayout />}>
+          <Route index element={<Home />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="about-us" element={<AboutUs />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
