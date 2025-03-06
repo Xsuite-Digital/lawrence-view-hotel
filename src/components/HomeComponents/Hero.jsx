fde06ae7b73bca7b68a3f176ab9c33cef5ead823
Mashhood-Rehman <mashhoodbutt47@gmail.com>
2025-03-06 13:48:31 +0500

    done

--- src/components/HomeComponents/Hero.jsx
+++ src/components/HomeComponents/Hero.jsx
@@ -32,7 +32,7 @@ const Hero = () => {
 
   return (
     <div
-      className="relative w-full bg-cover bg-center transition-all duration-1000 flex flex-col items-center bg-contain justify-center
+      className="relative w-full  bg-center transition-all duration-1000 flex flex-col items-center bg-contain justify-center
       h-[80vh] md:h-screen"
       style={{ backgroundImage: `url(${images[currentImageIndex]})` }}
     >
