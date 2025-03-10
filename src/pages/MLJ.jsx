793f2a6eaa144572041335d382bae86aacadf9ee
Mashhood-Rehman <mashhoodbutt47@gmail.com>
2025-03-10 14:27:35 +0500

    done

--- src/pages/MLJ.jsx
+++ src/pages/MLJ.jsx
@@ -1,18 +1,22 @@
-import React from 'react'
-import MLJHero from '../components/MLJC/MLJHero'
+import React, { Suspense } from 'react'
+const MLJHero = React.lazy(()=>import('../components/MLJC/MLJHero')) 
+const Bookingengine = React.lazy(()=> import('../components/BookingEngine/Bookingengine'));
 import MLJL from '../components/MLJC/MLJL'
 import CardSlider from '../components/HomeComponents/CardSlider'
 import HotelFeature from "../components/HomeComponents/HotelFeature";
 import Amenities from "../components/HomeComponents/Amenities";
 import ReasonsToBook from "../components/HomeComponents/ReasonsToBook"
-import Bookingengine from '../components/BookingEngine/Bookingengine';
+import Loader from '../Helpers/Loader';
 const MLJ = () => {
   return (
     <div>
+      <Suspense fallback={<Loader/>}>
+
       <MLJHero />
       <div className=" bg-black/50 border-t-4 border-b-4 border-[#c4a053]">
         <Bookingengine />
       </div>
+      </Suspense>
       <CardSlider />
       <ReasonsToBook />
       <Amenities />
